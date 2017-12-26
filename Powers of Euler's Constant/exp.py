9b3b56e5e8c3cdbd9e835846258c9e3a0107b1aa
Harvey Wang <Harvey@Harveys-MacBook-Pro.local>
2017-12-25 22:08:13 -0400

    exp.py

--- Powers of Euler's Constant/exp.py
+++ Powers of Euler's Constant/exp.py
@@ -4,12 +4,12 @@ def fact(x):
         return 1
     return x * fact(x - 1)
 
-# recursive function to evaluate e^x using power series 
+# recursive function to evaluate e^x using power series
 # the precision variable is the number of time the function will sum the series
-def pow(x, precision):
+def powS(x, precision):
     if precision == 0:
         return 1
-    return (x ** precision) / fact(precision) + pow(x, precision - 1)
+    return (x ** precision) / fact(precision) + powS(x, precision - 1)
 
 def close_enough(x1, x2):
     if abs(x1 - x2) < accuracy:
@@ -17,18 +17,18 @@ def close_enough(x1, x2):
     return False
 
 def myexp(x):
-    sum1 = pow(x, 1)
-    sum2 = pow(x, 2)
+    sum1 = powS(x, 1)
+    sum2 = powS(x, 2)
     i = 3
     # "keep adding terms until they no longer change the result"
     while not close_enough(sum1, sum2):
         sum1 = sum2
-        sum2 = pow(x, i)
-        i += 1 
+        sum2 = powS(x, i)
+        i += 1
     return sum2
 
 accuracy = 10 ** -7
 
 # print e^i where i is 0 to 20
-for i in range(21):
+for i in range(20):
     print(myexp(i))
